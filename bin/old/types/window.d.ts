024fef314dfe08f1c78bce9a678259d01d9a5faf
Allen Joseph <allenxavier45@gmail.com>
2025-03-23 22:53:22 +0530

    Reapply .gitignore rules

--- bin/old/types/window.d.ts
+++
@@ -1,7 +0,0 @@
-interface Window {
-    ethereum?: any;
-    handleGameOver?: (score: number) => void;
-    initializeGame?: () => void;
-    SalmonadObstacle?: any;
-    spawnSalmonadObstacle?: (obstacle: any) => void;
-} 
